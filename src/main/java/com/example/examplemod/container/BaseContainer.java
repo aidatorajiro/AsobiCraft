090e2d29a1bf183fcd9e0c512878827cb449874e
Aida Torajiro <kawarusosu@zoho.com>
2019-08-06 16:27:37 +0900

    Implement DrawFloatingSlots

--- src/main/java/com/example/examplemod/container/BaseContainer.java
+++ src/main/java/com/example/examplemod/container/BaseContainer.java
@@ -1,5 +1,7 @@
 package com.example.examplemod.container;
 
+import com.example.examplemod.slot.FloatingSlot;
+import com.example.examplemod.itemhandler.FloatingItemStackHandler;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.IInventory;
@@ -10,6 +12,31 @@ import net.minecraftforge.items.SlotItemHandler;
 public abstract class BaseContainer extends Container {
     @Override
     public abstract boolean canInteractWith(EntityPlayer playerIn);
+
+    public void drawFloatingSlots(FloatingItemStackHandler itemHandler, int offsetX, int offsetY, int shapeX) {
+        int index = 0;
+        for (int row = 0; row < (itemHandler.getSlots() / shapeX) + 1; ++row) {
+            int num_slots = row == (itemHandler.getSlots() / shapeX) ? itemHandler.getSlots() % shapeX : shapeX;
+            for (int col = 0; col < num_slots; ++col) {
+                int x = col * 18 + offsetX;
+                int y = row * 18 + offsetY;
+                this.addSlotToContainer(new FloatingSlot(itemHandler, index, x, y));
+                index++;
+            }
+        }
+    }
+
+    public void drawFloatingSlots(FloatingItemStackHandler itemHandler, int offsetX, int offsetY, int shapeX, int shapeY) {
+        int index = 0;
+        for (int row = 0; row < shapeY; ++row) {
+            for (int col = 0; col < shapeX; ++col) {
+                int x = col * 18 + offsetX;
+                int y = row * 18 + offsetY;
+                this.addSlotToContainer(new FloatingSlot(itemHandler, index, x, y));
+                index++;
+            }
+        }
+    }
 
     /**
      * Draw slots of given IItemHandler, only using the number of column of slots.
