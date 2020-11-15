1610a2932a8b5f5402f212340b5da32fac46dbf6
aidatorajiro <kawarusosu@zoho.com>
2020-11-16 02:02:46 +0900

    complete CalculatorBlock.java

--- src/main/java/com/example/examplemod/block/CalculatorBlock.java
+++ src/main/java/com/example/examplemod/block/CalculatorBlock.java
@@ -1,23 +1,24 @@
 package com.example.examplemod.block;
 
-import com.example.examplemod.ExampleMod;
 import com.example.examplemod.helper.BlockHelper;
 import com.example.examplemod.tileentity.CalculatorTileEntity;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.ITileEntityProvider;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.item.ItemStack;
+import net.minecraft.stats.Stats;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.BlockRenderLayer;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockAccess;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 
+import javax.annotation.Nullable;
 import java.util.Random;
 
 public class CalculatorBlock extends BaseBlock {
@@ -30,13 +31,24 @@ public class CalculatorBlock extends BaseBlock {
         return true;
     }
 
+    @Override
+    public void onBlockAdded(BlockState p_220082_1_, World worldIn, BlockPos pos, BlockState p_220082_4_, boolean p_220082_5_) {
+        super.onBlockAdded(p_220082_1_, worldIn, pos, p_220082_4_, p_220082_5_);
+        worldIn.getPendingBlockTicks().scheduleTick(pos, this, 10);
+    }
+
     /*
         Tile entity
      */
 
     @Override
-    public TileEntity createNewTileEntity(World world, int meta) {
+    public TileEntity createTileEntity(BlockState state, IBlockReader world) {
         return new CalculatorTileEntity();
+    }
+
+    @Override
+    public boolean hasTileEntity(BlockState state) {
+        return true;
     }
 
     /*
@@ -44,11 +56,17 @@ public class CalculatorBlock extends BaseBlock {
      */
 
     @Override
-    public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
+    public ActionResultType onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity entity, Hand hand, BlockRayTraceResult res) {
         if (!world.isRemote) {
-            player.openGui(ExampleMod.instance, 0, world, pos.getX(), pos.getY(), pos.getZ());
+            INamedContainerProvider cont = this.getContainer(state, world, pos);
+            if (cont != null) {
+                entity.openContainer(cont);
+                entity.addStat(Stats.OPEN_CHEST); // TODO
+            }
+            return ActionResultType.CONSUME;
+        } else {
+            return ActionResultType.SUCCESS;
         }
-        return true;
     }
 
     /*
@@ -56,69 +74,66 @@ public class CalculatorBlock extends BaseBlock {
      */
 
     @Override
-    public int tickRate(World worldIn) {
-        return 10;
-    }
-
-    @Override
-    public void updateTick(World world, BlockPos pos, IBlockState state, Random rand) {
-        super.updateTick(world, pos, state, rand);
+    public void tick(BlockState state, ServerWorld world, BlockPos pos, Random rand) {
+        super.tick(state, world, pos, rand);
         CalculatorTileEntity tile = (CalculatorTileEntity) world.getTileEntity(pos);
-        tile.updateTick(world, pos, state, rand);
+        tile.updateTick(state, world, pos, rand);
     }
 
     /*
         Partial Block
      */
 
-    @Override
-    public boolean isOpaqueCube(IBlockState state) {
-        return false;
-    }
+    //@Override  TODO
+    //public boolean isOpaqueCube(IBlockState state) {
+    //    return false;
+    //}
 
-    @Override
-    public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side) {
-        return true;
-    }
+    //@Override  TODO
+    //public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side) {
+    //    return true;
+    //}
 
-    @Override
-    public boolean isFullCube(IBlockState state) {
-        return false;
-    }
+    //@Override  TODO
+    //public boolean isFullCube(IBlockState state) {
+    //    return false;
+    //}
 
-    @Override
-    public BlockRenderLayer getBlockLayer() {
-        return BlockRenderLayer.CUTOUT;
-    }
+    //@Override  TODO
+    //public BlockRenderLayer getBlockLayer() {
+    //    return BlockRenderLayer.CUTOUT;
+    //}
 
-    @Override
-    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
-        return new AxisAlignedBB(1.0/16.0, 0, 1.0/16.0, 15.0/16.0, 2.0/16.0, 15.0/16.0);
-    }
+    //@Override  TODO
+    //public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+    //    return new AxisAlignedBB(1.0/16.0, 0, 1.0/16.0, 15.0/16.0, 2.0/16.0, 15.0/16.0);
+    //}
 
     /*
         NBT Items
      */
-    @Override
-    public int quantityDropped(IBlockState state, int fortune, Random random) {
-        if (fortune > 9) {
-            return 1;
-        } else {
-            return 0;
-        }
-    }
+    
+    //@Override  TODO
+    //public int quantityDropped(IBlockState state, int fortune, Random random) {
+    //    if (fortune > 9) {
+    //        return 1;
+    //    } else {
+    //        return 0;
+    //    }
+    //}
+
 
     @Override
-    public void onBlockPlacedBy(World world, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
-        super.onBlockPlacedBy(world, pos, state, placer, stack);
+    public void onBlockPlacedBy(World world, BlockPos pos, BlockState state, @Nullable LivingEntity entity, ItemStack stack) {
+        super.onBlockPlacedBy(world, pos, state, entity, stack);
         BlockHelper.restoreTE(world, pos, stack);
     }
 
     @Override
-    public void breakBlock(World worldIn, BlockPos pos, IBlockState state) {
-        if (!worldIn.isRemote) {
-            BlockHelper.spawnTE(worldIn, pos, this);
+    public void spawnAdditionalDrops(BlockState state, ServerWorld world, BlockPos pos, ItemStack stack) {
+        super.spawnAdditionalDrops(state, world, pos, stack);
+        if (!world.isRemote) {
+            BlockHelper.spawnTE(world, pos, this);
         }
-        super.breakBlock(worldIn, pos, state);
     }
 }
