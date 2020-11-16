acfc550f7991ceae3ebd699c35a44b3afbca4ec3
aidatorajiro <kawarusosu@zoho.com>
2020-11-16 11:57:14 +0900

    fix pickup-recipe script

--- src/main/java/com/example/examplemod/eventhandler/BlockPatternRecipeEventHandler.java
+++ src/main/java/com/example/examplemod/eventhandler/BlockPatternRecipeEventHandler.java
@@ -2,17 +2,16 @@ package com.example.examplemod.eventhandler;
 
 import com.example.examplemod.ModObjects;
 import com.example.examplemod.helper.ItemHelper;
-import com.example.examplemod.helper.WorldHelper;
 import com.example.examplemod.recipe.IBlockPatternRecipe;
-import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
-import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.Tuple;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraft.init.Blocks;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 /**
  * Event handler for overwriting vanilla behavior.
@@ -21,16 +20,16 @@ public class BlockPatternRecipeEventHandler {
     @SubscribeEvent
     public void pickupItem(EntityItemPickupEvent event) {
         Item item = event.getItem().getItem().getItem();
-        EntityPlayer player = event.getEntityPlayer();
-        BlockPos pos = new BlockPos(player.posX, player.posY, player.posZ);
-        World world = event.getEntityPlayer().getEntityWorld();
+        PlayerEntity player = event.getPlayer();
+        BlockPos pos = new BlockPos(player.getPosX(), player.getPosY(), player.getPosZ());
+        World world = event.getPlayer().getEntityWorld();
         for (IBlockPatternRecipe recipe : ModObjects.pickupRecipes) {
             Tuple<Boolean, Iterable<BlockPos>> result = recipe.triggerMatches(world, item, pos);
-            if (result.getFirst()) {
-                for (BlockPos delatePos : result.getSecond()) {
+            if (result.getA()) {
+                for (BlockPos delatePos : result.getB()) {
                     world.setBlockState(delatePos, Blocks.AIR.getDefaultState());
                 }
-                world.spawnParticle(EnumParticleTypes.EXPLOSION_HUGE, (double)pos.getX(), (double)pos.getY(), (double)pos.getZ(), 1.0D, 0.0D, 0.0D);
+                world.addParticle(ParticleTypes.EXPLOSION_EMITTER, (double)pos.getX(), (double)pos.getY(), (double)pos.getZ(), 1.0D, 0.0D, 0.0D);
                 ItemHelper.dropStackList(world, pos, recipe.getOutput(world, item, pos));
                 return;
             }
