a810a851224e78343fe5f67dad2561f77006ed80
aidatorajiro <kawarusosu@zoho.com>
2020-11-22 12:53:59 +0900

    decrease default INV_SIZE

--- src/main/java/com/example/examplemod/eventhandler/ModEventHandler.java
+++ src/main/java/com/example/examplemod/eventhandler/ModEventHandler.java
@@ -2,13 +2,16 @@ package com.example.examplemod.eventhandler;
 
 import com.example.examplemod.ModObjects;
 import com.example.examplemod.helper.ItemHelper;
+import com.example.examplemod.helper.WorldHelper;
 import com.example.examplemod.recipe.IBlockPatternRecipe;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.Tuple;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.Explosion;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.init.Blocks;
@@ -22,14 +25,16 @@ public class ModEventHandler {
         Item item = event.getItem().getItem().getItem();
         EntityPlayer player = event.getEntityPlayer();
         BlockPos pos = new BlockPos(player.posX, player.posY, player.posZ);
-        World world = event.getEntityPlayer().getEntityWorld();
+        WorldServer world = (WorldServer) event.getEntityPlayer().getEntityWorld();
         for (IBlockPatternRecipe recipe : ModObjects.pickupRecipes) {
             Tuple<Boolean, Iterable<BlockPos>> result = recipe.triggerMatches(world, item, pos);
             if (result.getFirst()) {
                 for (BlockPos delatePos : result.getSecond()) {
                     world.setBlockState(delatePos, Blocks.AIR.getDefaultState());
                 }
-                world.spawnParticle(EnumParticleTypes.EXPLOSION_HUGE, (double)pos.getX(), (double)pos.getY(), (double)pos.getZ(), 1.0D, 1.0D, 1.0D);
+                //WorldHelper.createExplosionParticle(world, pos, 0.5,0.5,0.5,10);
+                Explosion explosion = new Explosion(world, player, pos.getX(), pos.getY(), pos.getZ(), 2.0F, true, true);
+                explosion.doExplosionB(true);
                 ItemHelper.dropStackList(world, pos, recipe.getOutput(world, item, pos));
                 return;
             }
