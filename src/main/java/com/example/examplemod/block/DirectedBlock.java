3974a1ec4d5eba99b4587b6878e0d74323bf29bc
aidatorajiro <kawarusosu@zoho.com>
2020-12-01 14:43:28 +0900

    remove imports

--- src/main/java/com/example/examplemod/block/DirectedBlock.java
+++ src/main/java/com/example/examplemod/block/DirectedBlock.java
@@ -1,15 +1,11 @@
 package com.example.examplemod.block;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
